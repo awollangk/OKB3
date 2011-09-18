2af2ac8ceac031a906d0a47862e11d6aefb9f80e
Kalman Olah <admin@kalmanolah.net>
2011-09-18 23:33:13 +0200

    edited file

--- webfiles/config.inc.php
+++ webfiles/config.inc.php
@@ -7,9 +7,9 @@ $dbuser = 'root';
 $dbpass = 'password';
 //Your mysql database
 $dbname = 'dbname';
-//Your custom password. #####THIS MUST MATCH THE PHP PASSWORD IN CONFIG.PROPERTIES!#####
+//Your custom password. #####THIS MUST MATCH THE PHP PASSWORD IN CONFIG.YML!#####
 $phppass = '12345pass';
-//The prefix used by your forum tables. Let's say your the table where your usernames are stored in
+//The prefix used by your forum tables. Let's say your table your usernames are stored in
 // is called 'phpbb_users'...in this case the prefix is 'phpbb_'. set to $sqlprefix = ''; for no prefix.
 $sqlprefix = 'prefix_';
 ?>
